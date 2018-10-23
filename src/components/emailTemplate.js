ce61843bca55c18abaa42dcdab7671496a8ca8dc
grant <grant@taylorpond.com>
2018-10-23 09:58:24 -0700

    got content blocks mostly working, just needs the utm logic hashed out. other than that only footer is left for mvp

--- src/components/emailTemplate.js
+++ src/components/emailTemplate.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import html from './emailTemplate.json';
+import head from './headTemplate.json';
+// import head from './Template.json';
 
 class Template extends Component {
   render() {
 
-    console.log(this.props.data.content_data);
-    // const contentBlocks = this.props.data.contentData.map((item, i) =>
-    // <tr>
-    //  <td width="100%" align="center" valign="top" class="photo">
-    //   <div align="right" style="font-size: 0; line-height: 1;">
-    //    <a href={item.content_link} target="_blank" style="text-decoration:none;font-size:14px;font-family:arial;">
-    //     <img src="http://image.store.urbandecay.com/lib/fe8c13727c6d017d76/m/1/844890f2-e033-4cbf-a9e7-bab1f7ed3806.jpg" width="100%" alt="FIND YOUR VICE" border="0" style="display:block;color:#ffffff;" />
-    //    </a>
-    //   </div>
-    //  </td>
-    // </tr>
-    // );
+    const data = this.props.data.content_data
+    const contentBlocks = data.map((item, i) =>
+    '<tr>' +
+     '<td width="100%" align="center" valign="top" class="photo">' +
+      '<div align="right" style="font-size: 0; line-height: 1;">' +
+       '<a href="' + item.content_link + item.content_utm + '" target="_blank" style="text-decoration:none;font-size:14px;font-family:arial;">' +
+        '<img src="' + item.content_img_src + '" width="100%" alt="' + item.content_alt + '" border="0" style="display:block;color:#ffffff;"/>' +
+       '</a>' +
+      '</div>' +
+     '</td>' +
+    '</tr>'
+    );
+
+    console.log(contentBlocks);
+
+
 
     return (
       <div>
         <p>{
-          html.blockOne +
+          head.blockOne +
           this.props.data.header_data +
-          html.blockTwo
+          head.blockTwo +
+          contentBlocks
         }</p>
       </div>
     );
